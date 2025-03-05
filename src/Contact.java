17c8bae334a8eacdfc5f818330ccf79a2932dc69
kevinjt2001 <kjtorrealba@gmail.com>
2025-03-05 14:40:54 -0500

    Changes to Contact and PhonebookEntry classes

--- src/Contact.java
+++ src/Contact.java
@@ -1,3 +1,4 @@
+import java.util.List;
 
 /**
  * model a Contact Contact has a name & list of phonebook entries
@@ -9,5 +10,17 @@
  */
 
 class Contact {
+	private String name;
+	// create your list of PhonebookEntry
+	private List<PhonebookEntry> pbEntries;
 
+	public Contact(String name) {
+		this.name = name;
+		// initialize list
+	}
+
+	public void addPhonebookEntry(String number, String type) {
+		// create a pbEntry with the incoming num/type
+		// add that pbEntry to your list of pbEntries
+	}
 }
--- src/PhonebookEntry.java
+++ src/PhonebookEntry.java
@@ -9,5 +9,27 @@
  */
 
 class PhonebookEntry {
+	private String phoneNumber;
+	private String type;
 
+	public PhonebookEntry(String phoneNumber, String type) {
+		this.phoneNumber = phoneNumber;
+		this.type = type;
+	}
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setPhoneNumber(String number) {
+		phoneNumber = number;
+	}
+
+	public void setType(String t) {
+		type = t;
+	}
 }
